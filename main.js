b3c583f7bbd8c63fb2b731b8b682829ce4fb2cd7
Lutfu24 <lutfuhm@code.edu.az>
2023-03-22 00:11:33 +0400

    container completed

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+let btn_right = document.querySelector(".btn_right");
+let btn_left = document.querySelector(".btn_left");
+let advertising_first = document.querySelector(".advertising_first");
+let advertising_second = document.querySelector(".advertising_second");
 
+let arr = [advertising_first, advertising_second];
+
+window.addEventListener("DOMContentLoaded", () => {
+  arr[0].style.display = "block";
+});
+
+btn_right.addEventListener("click", () => {
+  arr[0].style.display = "none";
+  arr[1].style.display = "block";
+});
+
+btn_left.addEventListener("click", () => {
+  arr[1].style.display = "none";
+  arr[0].style.display = "block";
+});
